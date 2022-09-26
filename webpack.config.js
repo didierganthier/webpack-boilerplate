fb90591a4a82ae6de94f63a65ca4ff6afcef34d7
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-09-26 11:10:15 -0400

    Use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
+    mode: 'development',
    entry: {
      index: './src/index.js',
      print: './src/print.js',
@@ -12,9 +13,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       template: 'src/index.html'
     }),
   ],
+  devServer: {
+    static: './dist',
+  },
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
      clean: true,
    },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
  };
